158d3b9b0dbf45126e869d87638e72f7cec66b7c
Yohei Iino <wheatandcat@gmail.com>
2021-03-20 17:40:25 +0900

    update image (#13)

--- src/components/atoms/Image/stories.tsx
+++ src/components/atoms/Image/stories.tsx
@@ -6,17 +6,17 @@ import Image from './';
 storiesOf('atoms', module).add('Image', () => (
   <View>
     <Image
-      source={require('../../../img/categories/book.png')}
+      source={require('../../../img/categories/category_book.png')}
       width={50}
       height={50}
     />
     <Image
-      source={require('../../../img/categories/book.png')}
+      source={require('../../../img/categories/category_book.png')}
       width={100}
       height={100}
     />
     <Image
-      source={require('../../../img/categories/book.png')}
+      source={require('../../../img/categories/category_book.png')}
       width={150}
       height={150}
     />
