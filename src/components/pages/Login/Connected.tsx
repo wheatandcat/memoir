0f512f6b1715a26e19ef479b658c61f8a2527987
Yohei Iino <wheatandcat@gmail.com>
2021-05-02 00:57:13 +0900

    add: queries/createAuthUser.gql (#59)

--- src/components/pages/Login/Connected.tsx
+++ src/components/pages/Login/Connected.tsx
@@ -1,25 +1,57 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useCallback } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { storageKey } from 'lib/storage';
 import TemplateLogin from 'components/templates/Login/Page';
 import useFirebaseAuth from 'hooks/useFirebaseAuth';
+import useHomeItems from 'hooks/useHomeItems';
 import { useRecoilValue } from 'recoil';
 import { authUserState } from 'store/atoms';
+import {
+  useCreateAuthUserMutation,
+  CreateAuthUserMutationVariables,
+} from 'queries/api/index';
 
 type Props = {};
 
 export type ConnectedType = {};
 
 const Connected: React.FC<Props> = () => {
-  const { setup, onAppleLogin, onGoogleLogin } = useFirebaseAuth();
+  const { setup, onAppleLogin, onGoogleLogin, onLogout } = useFirebaseAuth();
+  const { refetch } = useHomeItems();
   const authUser = useRecoilValue(authUserState);
   const authenticated = !!authUser.uid;
   const navigation = useNavigation();
+  const [createAuthUserMutation] = useCreateAuthUserMutation({
+    async onCompleted() {
+      await refetch?.();
+      navigation.goBack();
+    },
+    async onError() {
+      // エラーになった場合はログアウトさせる
+      Alert.alert('エラー', 'ログインに失敗した');
+      onLogout();
+    },
+  });
+
+  const createAuthUser = useCallback(async () => {
+    const userID = await AsyncStorage.getItem(storageKey.USER_ID_KEY);
+
+    const variables: CreateAuthUserMutationVariables = {
+      input: {
+        id: String(userID),
+      },
+    };
+
+    createAuthUserMutation({ variables });
+  }, [createAuthUserMutation]);
 
   useEffect(() => {
     if (authenticated) {
-      navigation.goBack();
+      createAuthUser();
     }
-  }, [authenticated, navigation]);
+  }, [authenticated, navigation, createAuthUser]);
 
   if (!setup) {
     return null;
