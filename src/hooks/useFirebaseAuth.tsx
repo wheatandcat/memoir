4b0caf48c290c5b7113b186cff068365e434f35e
wheatandcat <wheatandcat@gmail.com>
2021-05-16 21:50:23 +0900

    connect: api by users.image

--- src/hooks/useFirebaseAuth.tsx
+++ src/hooks/useFirebaseAuth.tsx
@@ -1,8 +1,7 @@
-// FIXME: useIdTokenAuthRequestとRN debuggerを同時に使用するとエラーになるので開発時はこちらを使用
-// @see: https://github.com/expo/expo/issues/12712
-
 import * as AppleAuthentication from 'expo-apple-authentication';
 import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import { ResponseType } from 'expo-auth-session';
 import * as Crypto from 'expo-crypto';
 import { useRecoilValueLoadable, useRecoilState } from 'recoil';
 import { useCallback, useEffect, useState } from 'react';
@@ -38,8 +37,14 @@ const useFirebaseAuth = () => {
 
   const [setup, setSetup] = useState(false);
 
-  const request = {};
-  const onGoogleLogin = useCallback(() => {}, []);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    responseType: ResponseType.IdToken,
+    expoClientId: process.env.EXPO_GOOGLE_CLIENT_ID,
+  });
+
+  const onGoogleLogin = useCallback(() => {
+    promptAsync();
+  }, [promptAsync]);
 
   const setSession = useCallback(
     async (refresh = false) => {
@@ -72,6 +77,17 @@ const useFirebaseAuth = () => {
     },
     [setSession]
   );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+      firebaseLogin(credential);
+    } else if (response?.type === 'error') {
+      console.log('error:', response);
+      Alert.alert('ログインに失敗しました');
+    }
+  }, [response, firebaseLogin]);
 
   const onAppleLogin = useCallback(async () => {
     const nonce = nonceGen(32);
