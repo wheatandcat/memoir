187000d90856bcc5169b84d5f111791dde55b6f6
Yohei Iino <wheatandcat@gmail.com>
2021-08-04 23:18:40 +0900

    @expo-google-fontsを設定する (#101)
    * add: @expo-google-fonts
    
    * add: @expo-google-fonts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|@react-native-picker|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|@react-native-picker|@expo-google-fonts|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
   testRegex: '(spec|test)\\.[jt]sx?$',
   testPathIgnorePatterns: ['/node_modules/'],
--- src/components/atoms/Text/Text.tsx
+++ src/components/atoms/Text/Text.tsx
@@ -5,6 +5,9 @@ import { FontWeight, styleFontWeight } from 'lib/styledSystem/styleFontWeight';
 import { FontColor, styleFontColor } from 'lib/styledSystem/styleFontColor';
 import { styleFontLineHeight } from 'lib/styledSystem/styleFontLineHeight';
 import { useFonts } from 'expo-font';
+import { RobotoCondensed_700Bold } from '@expo-google-fonts/roboto-condensed';
+import { NotoSansJP_700Bold } from '@expo-google-fonts/noto-sans-jp';
+
 import {
   FontVariants,
   styleFontVariant,
@@ -25,8 +28,8 @@ type Props = TextProps & {
 
 const Text: React.FC<Props> = (props) => {
   let [fontsLoaded] = useFonts({
-    'RobotoCondensed-Bold': require('../../../../assets/RobotoCondensed-Bold.ttf'),
-    'NotoSansJP-Bold': require('../../../../assets/NotoSansJP-Bold.otf'),
+    'RobotoCondensed-Bold': RobotoCondensed_700Bold,
+    'NotoSansJP-Bold': NotoSansJP_700Bold,
   });
 
   if (!fontsLoaded) {
