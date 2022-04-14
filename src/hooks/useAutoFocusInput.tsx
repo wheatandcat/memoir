29f2613622c3cb6f856b9db64ab168fb5bf40a0b
wheatandcat <wheatandcat@gmail.com>
2022-04-14 19:48:38 +0900

    test

--- src/hooks/useAutoFocusInput.tsx
+++ src/hooks/useAutoFocusInput.tsx
@@ -1,21 +1,17 @@
 import { useRef } from 'react';
 import { TextInput } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
+import usePrevious from 'hooks/usePrevious';
 
 export default function useAutoFocusInput(autoFocus: boolean) {
   const ref = useRef<TextInput>(null);
   const timeout = useRef<number | null>(null);
+  const prevAutoFocus = usePrevious(autoFocus);
 
   useFocusEffect(() => {
-    if (autoFocus) {
+    if (autoFocus && !prevAutoFocus) {
       timeout.current = Number(setTimeout(() => ref.current?.focus(), 300));
     }
-
-    return () => {
-      if (timeout.current) {
-        clearTimeout(timeout.current);
-      }
-    };
   });
 
   return {
