212cd70dc49d2c778e02de4e7380fc007de88848
wheatandcat <wheatandcat@gmail.com>
2022-04-24 21:57:39 +0900

    update src/components/organisms/Setting/Licence/LicenceList.tsx

--- README.md
+++ README.md
@@ -15,36 +15,40 @@
 ### アプリ内コンフィグ
 
 #### レビュー環境
-```
+```bash
 $ base64 -i .env.development | pbcopy
 ```
 
 #### 本番環境
-```
+```bash
 $ base64 -i .env.production | pbcopy
 ```
 
 
 ### Expoコンフィグ
-```
+```bash
 $ base64 -i scripts/appConfig.js | pbcopy
 ```
 
+### ライセンスの出力
+```bash
+$ npx npm-license-crawler --dependencies --production --onlyDirectDependencies --omitVersion --json ./src/licenses.json
+```
 
 # コード生成
 
 ## 新規画面作成
-```
+```bash
 $ yarn hygen page new
 ```
 
 ## コンポーネント作成
-```
+```bash
 $ yarn hygen component new
 ```
 
 ## テストコード追加
-```
+```bash
 $ yarn hygen test new
 ```
 
@@ -52,19 +56,19 @@ $ yarn hygen test new
 
 ## 画像更新 
 
-```
+```bash
 $ yarn loki:update
 ```
 
 ## ローカル比較
 
-```
+```bash
 $ yarn loki:test
 ```
 
 ## reg-suit
 
-```
+```bash
 $ npx reg-suit run
 ```
 
@@ -72,12 +76,12 @@ $ npx reg-suit run
 
 ### iOS
 
-```
+```bash
 $ yarn ios:build
 ```
 
 ### Android
 
-```
+```bash
 $ yarn android:build
 ```
--- src/components/organisms/Setting/Licence/LicenceList.tsx
+++ src/components/organisms/Setting/Licence/LicenceList.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 import ListItem from 'components/molecules/ListItem';
 import View from 'components/atoms/View';
+import licenses from '../../../../licenses.json';
 import * as Linking from 'expo-linking';
 
 type Props = {};
@@ -13,9 +14,65 @@ const items = [
 ];
 
 const LicenceList: React.FC<Props> = () => {
+  const licenseKeys = Object.keys(licenses);
+
+  const licenseItems = licenseKeys
+    .map((key) => {
+      const v = licenses as any;
+
+      return {
+        name: key,
+        url: v?.[key]?.licenseUrl || '',
+      };
+    })
+    .filter((v) => {
+      if (!v.url || !v.name) {
+        return false;
+      }
+
+      if (
+        v.name.includes('@expo') ||
+        v.name.includes('expo-') ||
+        v.name.includes('@types') ||
+        v.name.includes('graphql-') ||
+        v.name.includes('@react-native-community') ||
+        v.name.includes('react-dom') ||
+        v.name.includes('@react-navigation/stack') ||
+        v.name.includes('react-native-screens') ||
+        v.name.includes('react-native-safe-area-context')
+      ) {
+        return false;
+      }
+
+      return true;
+    })
+    .map((v) => {
+      if (
+        v.name === 'recoil' ||
+        v.name === 'react-native' ||
+        v.name === '@react-navigation/native' ||
+        v.name === 'react-native-gesture-handler' ||
+        v.name === 'graphql' ||
+        v.name === 'react' ||
+        v.name === 'uuid'
+      ) {
+        v.url = v.url.replace('master', 'main');
+      } else if (v.name === 'react-native-snap-carousel') {
+        v.url =
+          'https://github.com/archriss/react-native-snap-carousel/raw/master/LICENSE';
+      } else if (v.name === 'expo') {
+        v.url = 'https://github.com/expo/expo/raw/main/LICENSE';
+      } else if (v.name === 'firebase') {
+        v.url =
+          'https://raw.githubusercontent.com/firebase/firebase-js-sdk/master/LICENSE';
+      }
+
+      return v;
+    });
+
   return (
     <View>
-      {items.map((v, index) => (
+      {[...items, ...licenseItems].map((v, index) => (
         <ListItem
           key={v.name}
           title={v.name}
