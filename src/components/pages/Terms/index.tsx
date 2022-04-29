101d3520d5060c5e00b385b88e51a4615ab04247
wheatandcat <wheatandcat@gmail.com>
2022-04-29 21:53:21 +0900

    update url

--- src/components/pages/Terms/index.tsx
+++ src/components/pages/Terms/index.tsx
@@ -17,7 +17,7 @@ const Terms: React.FC<Props> = () => {
   return (
     <WebView
       style={styles.root}
-      source={{ uri: 'https://memoir-lp.vercel.app/terms?app=true' }}
+      source={{ uri: 'https://memoir-app.dev/terms/app' }}
     />
   );
 };
