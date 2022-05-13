415b60047abe151cc930585a11194d7db5a3b909
Yohei Iino <wheatandcat@gmail.com>
2022-05-13 21:53:37 +0900

    fix text (#210)

--- src/components/organisms/Home/NotFound.tsx
+++ src/components/organisms/Home/NotFound.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import NotFoundIcon from 'components/molecules/NotFound/Icon';
 import View from 'components/atoms/View';
 import Text from 'components/atoms/Text';
+import dayjs from 'lib/dayjs';
 
 type Props = {
   date: string;
@@ -12,7 +13,11 @@ const NotFound: React.FC<Props> = (props) => {
   return (
     <View style={styles.root}>
       <View>
-        <Text color="baseDark">今日のタスクは{'\n'}まだありません</Text>
+        <Text color="baseDark">
+          {dayjs(props.date).isSame(dayjs(), 'day') ? '今日' : 'この日'}
+          のタスクは
+          {'\n'}まだありません
+        </Text>
       </View>
       <View mt={4}>
         <NotFoundIcon date={props.date} />
