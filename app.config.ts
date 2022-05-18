ff533e3973ce6ce6314fb7705249608f30f4bd5e
wheatandcat <wheatandcat@gmail.com>
2022-05-18 23:03:33 +0900

    update config

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,53 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ja';
-import { appConfig } from './appConfig';
+require('dotenv').config();
 
 const version: string = '1.2.0';
 const unix = dayjs().unix().toString();
 
+const appConfig = () => {
+  const ios: any = {};
+  const android: any = {};
+  if (process.env.IOS_GOOGLE_SERVICES_FILE) {
+    ios.googleServicesFile = process.env.IOS_GOOGLE_SERVICES_FILE;
+  }
+  if (process.env.ANDROID_GOOGLE_SERVICES_FILE) {
+    android.googleServicesFile = process.env.IOS_GOOGLE_SERVICES_FILE;
+  }
+
+  return { ios, android };
+};
+
+console.log('process.env.API_HOST:', process.env.API_HOST);
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
+  extra: {
+    API_HOST: process.env.API_HOST,
+    FIRE_BASE_API_KEY: process.env.FIRE_BASE_API_KEY,
+    FIRE_BASE_AUTH_DOMAIN: process.env.FIRE_BASE_AUTH_DOMAIN,
+    FIRE_BASE_PROJECT_ID: process.env.FIRE_BASE_PROJECT_ID,
+    FIRE_BASE_STORAGE_BUCKET: process.env.FIRE_BASE_STORAGE_BUCKET,
+    FIRE_BASE_MESSAGING_SENDER_ID: process.env.FIRE_BASE_MESSAGING_SENDER_ID,
+    FIRE_BASE_APP_ID: process.env.FIRE_BASE_APP_ID,
+    FIRE_BASE_MEASUREMENT_ID: process.env.FIRE_BASE_MEASUREMENT_ID,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    EXPO_GOOGLE_CLIENT_ID: process.env.EXPO_GOOGLE_CLIENT_ID,
+    ANDROID_GOOGLE_CLIENT_ID: process.env.ANDROID_GOOGLE_CLIENT_ID,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    STORAGE_URL: process.env.STORAGE_URL,
+    IMAGE_MERGE_API: process.env.IMAGE_MERGE_API,
+    INQUIRY_API: process.env.INQUIRY_API,
+    ENV: process.env.NODE_ENV,
+  },
   name: 'memoir',
   scheme: 'com.wheatandcat.memoir',
   slug: 'memoir',
   version,
   orientation: 'portrait',
   icon: './assets/ios-icon.png',
+  jsEngine: 'hermes',
   splash: {
     image: './assets/splash.png',
     resizeMode: 'contain',
@@ -70,9 +104,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       {
         file: 'sentry-expo/upload-sourcemaps',
         config: {
-          organization: appConfig().SENTRY_ORGANIZATION,
-          project: appConfig().SENTRY_PROJECT,
-          authToken: appConfig().SENTRY_AUTH_TOKEN,
+          organization: process.env.SENTRY_ORGANIZATION,
+          project: process.env.SENTRY_PROJECT,
+          authToken: process.env.SENTRY_AUTH_TOKEN,
         },
       },
     ],
