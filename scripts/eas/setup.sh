e2868b762f2c71a34b400904037edb000beb24c2
wheatandcat <wheatandcat@gmail.com>
2022-05-19 09:10:56 +0900

    add eas setup

--- scripts/eas/setup.sh
+++ scripts/eas/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo $IOS_INFO_PLIST | base64 --decode > ./ios/GoogleService-Info_eas.plist
-echo $ANDROID_GOOGLE_SERVICES_JSON | base64 --decode > ./android/google-services_eas.json
-ls ./ios
-ls ./android
-pwd
+echo $IOS_INFO_PLIST | base64 --decode > ./ios/GoogleService-Info.plist
+echo $ANDROID_GOOGLE_SERVICES_JSON | base64 --decode > ./android/google-services.json
+echo ls ./ios
+echo ls ./android
+echo pwd
