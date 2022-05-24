1fcb72be21d708263f32e467d64becf591a61571
wheatandcat <wheatandcat@gmail.com>
2022-05-24 22:57:11 +0900

    update

--- scripts/eas/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo $IOS_INFO_PLIST | base64 --decode > ./ios/GoogleService-Info.plist
-echo $ANDROID_GOOGLE_SERVICES_JSON | base64 --decode > ./android/google-services.json
-echo ls ./ios
-echo ls ./android
-echo pwd
