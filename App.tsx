c09b839d090769f3eebc493b8b233d1fd926ef22
wheatandcat <wheatandcat@gmail.com>
2022-05-29 13:32:28 +0900

    update

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import { RobotoCondensed_700Bold } from '@expo-google-fonts/roboto-condensed';
 import { NotoSansJP_700Bold } from '@expo-google-fonts/noto-sans-jp';
 import { LogBox } from 'react-native';
@@ -24,16 +25,58 @@ LogBox.ignoreLogs([
 ]);
 
 const AppContainer = () => {
-  let [fontsLoaded] = useFonts({
-    'RobotoCondensed-Bold': RobotoCondensed_700Bold,
-    'NotoSansJP-Bold': NotoSansJP_700Bold,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'RobotoCondensed-Bold': RobotoCondensed_700Bold,
+          'NotoSansJP-Bold': NotoSansJP_700Bold,
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
-  return <App />;
+  return (
+    <View style={styles.root} onLayout={onLayoutRootView}>
+      <App />
+    </View>
+  );
 };
 
 export default AppContainer;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
