1a665dc419fbe52fc398ac6e5116c48c86d11912
wheatandcat <wheatandcat@gmail.com>
2022-06-18 15:53:28 +0900

    update

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ if (process.env.APP_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const version: string = '1.2.0';
+const version: string = '1.3.0';
 const unix = dayjs().unix().toString();
 
 const appConfig = () => {
