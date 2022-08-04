4e05f3348180f7a655587ce368d76ad8cc0f277d
wheatandcat <wheatandcat@gmail.com>
2022-08-04 22:24:05 +0900

    update: loopClonesPerSide

--- src/components/molecules/DateInput/Days.tsx
+++ src/components/molecules/DateInput/Days.tsx
@@ -128,7 +128,7 @@ const DayInput: React.FC<Props> = (props) => {
       inactiveSlideOpacity={1.0}
       inactiveSlideScale={1.0}
       loop
-      loopClonesPerSide={10}
+      loopClonesPerSide={dayItems.length}
       removeClippedSubviews
     />
   );
