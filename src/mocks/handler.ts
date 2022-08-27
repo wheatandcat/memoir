91cc1c402d154f6c4f893e8b75b0bdf1b596acb1
wheatandcat <wheatandcat@gmail.com>
2022-08-27 23:10:19 +0900

    add: typescript-mock-data

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,13 @@
 import { graphql } from 'msw';
 import { ItemDocument } from 'queries/api/index';
-import { item } from '__mockData__/item';
+import { anItem } from 'queries/api/mocks';
 
 export const handlers = [
   graphql.query(ItemDocument, (req, res, ctx) => {
     return res(
       ctx.data({
         item: {
-          ...item(),
+          ...anItem(),
           id: req.variables.id,
         },
       })
