4ca58d550cec377a881c8996455db21d316eb677
wheatandcat <wheatandcat@gmail.com>
2023-07-09 23:11:35 +0900

    update storybook v6

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-actions',
+  ],
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,35 @@
+import Config from 'containers/Config';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RecoilRoot } from 'recoil';
+import Notification from './Notification';
+
+const Stack = createStackNavigator();
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ActionSheetProvider>
+      <Config>
+        <Notification>
+          <RecoilRoot>
+            <NavigationContainer independent={true}>
+              <Stack.Navigator>
+                <Stack.Screen name="StorybookUIRoot" component={Story} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </RecoilRoot>
+        </Notification>
+      </Config>
+    </ActionSheetProvider>
+  ),
+];
