1994a39ef20253ce2089c98c05f0d1c12beba6a9
wheatandcat <wheatandcat@gmail.com>
2023-07-29 17:02:15 +0900

    fix

--- src/components/molecules/DateInput/Months.tsx
+++ src/components/molecules/DateInput/Months.tsx
@@ -1,14 +1,14 @@
-import React, { memo, useRef, useEffect, useCallback, useState } from 'react';
+import React, { memo, useCallback, useRef, useState } from 'react';
 import {
   StyleSheet,
   TouchableWithoutFeedback,
-  Dimensions,
-  NativeSyntheticEvent,
+  useWindowDimensions,
+  ListRenderItemInfo,
 } from 'react-native';
 import Text from 'components/atoms/Text';
 import View from 'components/atoms/View';
 import theme from 'config/theme';
-import PagerView from 'react-native-pager-view';
+import Carousel from 'react-native-snap-carousel';
 
 type Month = {
   label: string;
@@ -21,122 +21,79 @@ type Props = {
   onPress: (month: string) => void;
 };
 
-const SHOW_ITEMS = 5;
-const { width } = Dimensions.get('window');
-const ITEM_WIDTH = width / SHOW_ITEMS;
+type RenderedItem = {
+  value: string;
+  month: Month;
+  onPress: (month: string) => void;
+};
+
+type RenderedItemProps = ListRenderItemInfo<RenderedItem>;
+
+const renderItem: React.FC<RenderedItemProps> = ({ item }) => {
+  return (
+    <TouchableWithoutFeedback
+      key={item.month.value}
+      onPress={() => item.onPress(`00${item.month.value}`.slice(-2))}
+    >
+      <View style={styles.monthItem}>
+        <Text
+          color={
+            String(item.month.value) === item.value ? 'primary' : 'secondary'
+          }
+        >
+          {item.month.label}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 const MonthInput: React.FC<Props> = (props) => {
-  const months = useCallback((): Month[] => {
+  const months = (): Month[] => {
     const index = Number(props.month);
     const first = props.months.slice(index - 1, 12);
     const last = props.months.slice(0, index - 1);
     return [...first, ...last];
-  }, [props.months, props.month]);
-
-  const generateData = useCallback((originalData: any, chunkSize: number) => {
-    const result = [];
-    let i = 0;
-
-    while (true) {
-      const chunk: Month[] = [];
-
-      for (let j = 0; j < chunkSize; j++) {
-        chunk.push(originalData[(i + j) % originalData.length]);
-      }
-
-      if (result.length > 0 && chunk[0].value === result[0][0].value) {
-        break;
-      }
-
-      result.push(chunk);
-      i += chunkSize;
-    }
-
-    return result;
-  }, []);
-
-  const pagerRef = useRef<PagerView>(null);
-  const originalData = months();
-  const gdata = generateData(originalData, SHOW_ITEMS);
-
-  const [data] = useState(gdata);
-  const pages = [...data.slice(-1), ...data, ...data.slice(0, 1)];
-
-  useEffect(() => {
-    if (pagerRef.current) {
-      pagerRef.current.setPageWithoutAnimation(1);
-    }
-  }, []);
-
-  const handlePageSelected = (e: NativeSyntheticEvent<any>) => {
-    if (pagerRef.current) {
-      const page = e.nativeEvent.position;
-      if (page === 0) {
-        pagerRef.current.setPageWithoutAnimation(page + originalData.length);
-      } else if (page > originalData.length) {
-        pagerRef.current.setPageWithoutAnimation(page - originalData.length);
-      }
-    }
   };
 
+  const [monthItems] = useState(months());
+
+  const carouselRef = useRef<Carousel<any>>(null);
+  const windowWidth = useWindowDimensions().width;
+
+  const renderItemCall = useCallback(
+    (item: ListRenderItemInfo<RenderedItem>) => {
+      return renderItem(item);
+    },
+    []
+  );
+
   return (
-    <PagerView
-      ref={pagerRef}
-      style={styles.pagerView}
-      initialPage={1}
-      onPageSelected={handlePageSelected}
-      pageMargin={10}
-    >
-      {pages.map((page, pageIndex) => (
-        <View key={pageIndex} style={styles.page}>
-          {page.map((item, itemIndex) => (
-            <MonthItem
-              key={itemIndex}
-              value={item.value}
-              label={item.label}
-              selected={String(item.value) === props.month}
-              onPress={props.onPress}
-            />
-          ))}
-        </View>
-      ))}
-    </PagerView>
+    <Carousel
+      ref={carouselRef}
+      data={monthItems.map((v) => ({
+        value: props.month,
+        month: v,
+        onPress: props.onPress,
+      }))}
+      renderItem={renderItemCall}
+      sliderWidth={windowWidth}
+      itemWidth={75}
+      layout="default"
+      inactiveSlideOpacity={1.0}
+      inactiveSlideScale={1.0}
+      activeSlideAlignment="start"
+      loop
+      loopClonesPerSide={monthItems.length}
+      removeClippedSubviews
+    />
   );
 };
 
 export default memo(MonthInput);
 
-type MonthItemProps = Month & {
-  selected: boolean;
-  onPress: (month: string) => void;
-};
-
-const MonthItem: React.FC<MonthItemProps> = memo((props) => {
-  return (
-    <TouchableWithoutFeedback
-      key={props.value}
-      onPress={() => props.onPress(`00${props.value}`.slice(-2))}
-    >
-      <View style={styles.monthItem}>
-        <Text color={props.selected ? 'primary' : 'secondary'}>
-          {props.label}
-        </Text>
-      </View>
-    </TouchableWithoutFeedback>
-  );
-});
-
 const styles = StyleSheet.create({
   monthItem: {
     paddingRight: theme().space(3),
-    width: ITEM_WIDTH,
-  },
-  pagerView: {
-    flex: 1,
-    paddingTop: 25,
-  },
-  page: {
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
   },
 });
