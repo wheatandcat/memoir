7e1afc02165225c30f3365cd020ec477a6046ce1
wheatandcat <wheatandcat@gmail.com>
2025-03-30 11:50:04 +0900

    add: sentry

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-// This replaces `const { getDefaultConfig } = require('expo/metro-config');`
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
-
-// This replaces `const config = getDefaultConfig(__dirname);`
-const config = getSentryExpoConfig(__dirname);
-
-module.exports = config;
