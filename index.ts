7eee202acb252a559678947e96ea03a5eb9b150c
wheatandcat <wheatandcat@gmail.com>
2025-05-03 22:52:01 +0900

    fix

--- index.ts
+++ index.ts
@@ -4,10 +4,8 @@
  */
 if (process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true") {
   // Storybook を起動
-  console.log("Storybook を起動");
   require("./.storybook");
 } else {
   // 通常アプリ（expo-router）
-  console.log("通常アプリ（expo-router）を起動");
   require("expo-router/entry");
 }
