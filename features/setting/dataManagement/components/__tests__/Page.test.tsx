58c8dfb1d43c9a8850246ae47fe48f1d330e3501
wheatandcat <wheatandcat@gmail.com>
2025-08-11 22:47:48 +0900

    fix

--- features/setting/dataManagement/components/__tests__/Page.test.tsx
+++ features/setting/dataManagement/components/__tests__/Page.test.tsx
@@ -4,18 +4,6 @@ import React from "react";
 import Page from "../Page";
 import type { Props } from "../Page";
 
-jest.mock("react-native-safe-area-context", () => {
-  const inset = { top: 0, right: 0, bottom: 0, left: 0 };
-  return {
-    SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
-    SafeAreaConsumer: jest
-      .fn()
-      .mockImplementation(({ children }) => children(inset)),
-    SafeAreaView: jest.fn().mockImplementation(({ children }) => children),
-    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
-  };
-});
-
 const propsData = (): Props => ({
   loading: false,
   disabledDeleteButton: false,
--- setupTests.js
+++ setupTests.js
@@ -48,6 +48,18 @@ jest.mock("react-native/Libraries/Linking/Linking", () => ({
   getInitialURL: jest.fn(),
 }));
 
+jest.mock("react-native-safe-area-context", () => {
+  const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+  return {
+    SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
+    SafeAreaConsumer: jest
+      .fn()
+      .mockImplementation(({ children }) => children(inset)),
+    SafeAreaView: jest.fn().mockImplementation(({ children }) => children),
+    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+  };
+});
+
 jest.mock("@react-navigation/native", () => {
   const actualNav = jest.requireActual("@react-navigation/native");
   return {
