445528fd523c479e860b5e1262556780d9cfc634
wheatandcat <wheatandcat@gmail.com>
2025-08-18 21:00:24 +0900

    fix

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import "dayjs/locale/ja";
 import "dotenv/config";
 
-const version: string = "1.9.0";
+const version: string = "1.9.1";
 const unix = dayjs().unix().toString();
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
